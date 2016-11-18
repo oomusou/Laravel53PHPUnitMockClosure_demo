b6d6e62da28e6609b4e9f38726978c25366332cb
Sam.IO.Hsiao <sam.io.hsiao@benq.com>
2016-11-18 11:52:48 +0800

    將 ShippingService 的 Closure type hint 拿掉

--- app/Services/ShippingService.php
+++ app/Services/ShippingService.php
@@ -4,11 +4,9 @@ declare(strict_types = 1);
 
 namespace App\Services;
 
-use Closure;
-
 class ShippingService
 {
-    public function calculateFee(int $weight, Closure $logistics) : int
+    public function calculateFee(int $weight, $logistics) : int
     {
         return $logistics($weight);
     }

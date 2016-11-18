5d7dc404a9b253cd7b4c74230c5b3461020e6e00
Sam.IO.Hsiao <sam.io.hsiao@benq.com>
2016-11-18 11:27:11 +0800

    ShippingService 使用 closure

--- app/Services/ShippingService.php
+++ app/Services/ShippingService.php
@@ -4,10 +4,12 @@ declare(strict_types = 1);
 
 namespace App\Services;
 
+use Closure;
+
 class ShippingService
 {
-    public function calculateFee(int $weight, LogisticsInterface $logistics) : int
+    public function calculateFee(int $weight, Closure $logistics) : int
     {
-        return $logistics->calculateFee($weight);
+        return $logistics($weight);
     }
 }
